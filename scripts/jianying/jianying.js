50424e34da50220d07fe69467336987803929c7c
buehen <74601640+buehen@users.noreply.github.com>
2021-06-28 17:51:01 +0800

    1

--- scripts/jianying/jianying.js
+++ scripts/jianying/jianying.js
@@ -2,8 +2,10 @@
 
 简影 unlock Vip by 渤涵
 
+obj.vip_type = "com.jyys.jianmovie.yearvipsubscription",
+
 [rewrite_local]
-^https:\/\/api\.jianmovie\.com\/cardinfo url script-response-body buehen0426/Scripts/jianying/jianying.js
+^https:\/\/api\.jianmovie\.com\/(cardinfo|expense) url script-response-body buehen0426/Scripts/jianying/jianying.js
 
 [MITM]
 hostname = api.jianmovie.com
@@ -12,8 +14,10 @@ hostname = api.jianmovie.com
 
 var obj = JSON.parse($response.body);
 obj.is_vip = 1,
-obj.vip_type = "com.jyys.jianmovie.yearvipsubscription",
+obj.vip_type = 1,
 obj.vip_expire_time = 1999999999,
+obj.success = 1,
+obj.error_msg = ""
 
 
 $done({body: JSON.stringify(obj)});
